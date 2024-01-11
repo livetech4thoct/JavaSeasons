698c04479f19944ec8de9f76fe626e6a4bf7ab0a
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-01-11 11:46:14 +0530

    String programs on Jan11th

---
+++ src/jan11th/StringDemo1.java
@@ -0,0 +1,26 @@
+package jan11th;
+
+public class StringDemo1 {
+
+	public static void main(String[] args) {
+		
+		String s1="LiveTech";
+		
+		String s2=new String("LiveTech");
+		
+		String s3="LiveTech";
+		
+		//System.out.println(s1==s2);
+		
+		//System.out.println(s1.equals(s2));
+		
+		System.out.println(s1==s3);
+		
+		
+		
+		
+		
+
+	}
+
+}
